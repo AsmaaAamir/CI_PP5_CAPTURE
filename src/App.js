8fa463ecc8133aeb8711f56628efb8a705f29014
AsmaaAamir <>
2023-06-01 07:07:00 +0000

    trying to center the leanding page message

--- src/App.js
+++ src/App.js
@@ -11,11 +11,10 @@ import Welcomepage from "./components/Welcomepage";
 function App() {
   return (
     <div className={styles.App}>
-        <Home />
             <Switch>
-                <Route exact path="/" render={ () => <Welcomepage/>  }/>
+                <Route exact path="/" render={ () => <Welcomepage/> }/>
                 <Route exact path="/home" render={ () => <Home/> }/>
-                <Route exact path="/profile" render={ () => <h1>Profile</h1> } />
+                <Route exact path="/profile" render={ () => <h1>Profile</h1> }/>
                 <Route exact path="/addpost" render={ () => <h1>Add Post</h1> }/>
                 <Route exact path="/signin" render={ () => <h1>Sign In</h1> }/>
                 <Route exact path="/signup" render={ () => <h1>Sign Up</h1> }/>
--- src/components/Welcomepage.js
+++ src/components/Welcomepage.js
@@ -1,36 +1,58 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from '../assets/logo.jpg'
-import { Card, button } from "react-bootstrap";
+import logo from '../assets/new_logo.png';
+import { Card, Container } from "react-bootstrap";
+import styles from '../styles/WelcomePage.module.css';
 
 /* Welcome page wih option ot sign in and sign up */
 
 const Welcomepage = () => {
     return (
-        <Card className="bg-dark text-white">
-            <Card.ImgOverlay>
-                <Card.Title>
-                    <img src={logo} alt="capture logo" />
+        <Container className={styles.WelcomeCard}>
+            <div className={styles.Card}>
+                <div className={styles.Heading}>
+                    <img src={logo} alt="capture logo" height="200"/>
                     <h1>Capture</h1>
-                </Card.Title>
-                <Card.Text>
-                    Welcome to our community!
+                </div>
+                <div className={styles.Text}>
+                    Welcome!
                     <br/>
                     Capture the moments and share it with your loved one and the world. Become the next inspiration for someone..!
                     <br/>
-                    <NavLink  to="/signin">
-                        <button>Sign In </button>
+                    <NavLink  to="/signin" >
+                        <button className={styles.WelcomeButton} >Sign In </button>
                     </NavLink>
                     <p> or </p>
-                    <NavLink to="/signup">
-                        <button>Sign Up </button>
-                    </NavLink>
-                
-                </Card.Text>
-            </Card.ImgOverlay>
-        </Card>
+                    <NavLink to="/signup" >
+                        <button className={styles.WelcomeButton}>Sign Up </button>
+                    </NavLink>   
+                </div>
+            </div>
+        </Container>
     );
 };
 
 
 export default Welcomepage;
+
+<Card  className={styles.WelcomeCard}>
+<Card.ImgOverlay  >
+    <Card.Title className={styles.Heading}>
+        <img src={logo} alt="capture logo" height="200"/>
+        <h1>Capture</h1>
+    </Card.Title>
+    <Card.Text className={styles.Text}>
+        Welcome!
+        <br/>
+        Capture the moments and share it with your loved one and the world. Become the next inspiration for someone..!
+        <br/>
+        <NavLink  to="/signin" >
+            <button className={styles.WelcomeButton} >Sign In </button>
+        </NavLink>
+        <p> or </p>
+        <NavLink to="/signup" >
+            <button className={styles.WelcomeButton}>Sign Up </button>
+        </NavLink>
+    </Card.Text>
+</Card.ImgOverlay>
+</Card>
