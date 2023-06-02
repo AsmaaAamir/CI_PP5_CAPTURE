dd579d1cda7811a350295294213ab81fece79022
AsmaaAamir <>
2023-06-02 10:13:28 +0000

    added the nav link back

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -31,11 +31,11 @@ const NavBar = () => {
     const loggedInIcons = <> {currentUser?.username} </>;
     const loggedOutIcons =(
         <>
-           <NavLink  to="/"className={styles.Icon}>
+           <NavLink  to="/signin"className={styles.Icon}>
                 <i class="fas fa-sign-in-alt"></i>
                 <span>Sign In </span>
             </NavLink>
-            <NavLink to="/signin" className={styles.Icon}>
+            <NavLink to="/signup" className={styles.Icon}>
                 <i class="fas fa-user-plus"></i>
                 <span>Sign Up </span>
             </NavLink>
