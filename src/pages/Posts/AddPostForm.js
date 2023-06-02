df2ba0a3d1be9112c6d1479fc212fe8fe365e65a
AsmaaAamir <>
2023-06-02 08:05:06 +0000

    added category option for country

--- src/pages/Posts/AddPostForm.js
+++ src/pages/Posts/AddPostForm.js
@@ -1,2 +1,110 @@
-import React , {useState} from "react";
+import React, {useState} from "react";
+import {Form, Button, Row, Col, Container, Image} from "react-bootstrap";
+import Upload from "../../assets/upload.png";
+import styles from "../../styles/AddPost.module.css";
+import Asset from "../../components/Asset";
+import { useHistory } from "react-router-dom";
 
+function AddPostForm(){
+    const [errors, setErrors] = useState({});
+
+    const [postData, setPostData ] = useState ({
+        title: "",
+        description: "",
+        category: "",
+        image: "",
+    });
+
+    const { title, description, category, image } = postData;
+    
+    const imageInput = useRef(null)
+
+    const history = useHistory();
+
+    const handleChange = (e) =>{
+        setPostData({
+            ...postData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleChangeImage = (e) => {
+        if (e.target.files.lenth) {
+            URL.revokeObjectURL(image);
+            setPostData({
+                ...postData,
+                image: URL.createObjectURL(e.target.files[0]),
+            });
+        }
+    };
+
+    const textFields =(
+        <div className="text-center">
+            <Form.Group>
+                <Form.Label>Title</Form.Label>
+                <Form.Control 
+                    type="text"
+                    name="title"
+                    value={title}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {errors.title?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+             <Form.Group>
+                <Form.Label>Description</Form.Label>
+                <Form.Control 
+                    type="text"
+                    name="description"
+                    rows={8}
+                    value={description}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {errors.description?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+            <Form.Group>
+                <Form.Label>Category</Form.Label>
+                <Form.Control 
+                    as="select"
+                    name="category"
+                    value={category}
+                    onChange={handleChange}
+                >
+                <option>Please select the Country</option>
+                <option value="India">India</option> <option value="China">China</option> <option value="United States">United States</option>
+                <option value="Bangladesh">Bangladesh</option> <option value="Afghanistan">Afghanistan</option> <option value="Nigeria">Nigeria</option>
+                <option value="Russia">Russia</option> <option value="Yemen">Yemen</option> <option value="Indonesia">Indonesia</option>
+                <option value="Brazil">Brazil</option> <option value="Mali">Mali</option> <option value="Philippines">Philippines</option>
+                <option value="Mexico">Mexico</option> <option value="Mozambique">Mozambique</option> <option value="Egypt">Egypt</option>
+                <option value="United Kingdom">United Kingdom</option> <option value="Poland">Poland</option> <option value="France">France'</option>
+                <option value="Ethiopoa">Ethiopoa</option> <option value="Saudi Arabia">Saudi Arabia</option> <option value="South Africa">South Africa</option>
+                <option value="Iran">Iran</option> <option value="Algeria">Algeria</option> <option value="Myanmar">Myanmar</option>
+                <option value="Tanzania">Tanzania</option> <option value="Thailand">Thailand</option> <option value="Ghana">Ghana</option>
+                <option value="South Korea">South Korea</option> <option value="Germany">Germany</option> <option value="Uganda">Uganda</option>
+                <option value="Angola">Angola</option> <option value="Japan">Japan</option> <option value="Mauritania">Mauritania</option>
+                <option value="Peru">Peru</option> <option value="Pakistan">Pakistan</option> <option value="Uzbekistan">Uzbekistan</option>
+                <option value="Sudan'">Sudan</option> <option value="Argentina">Argentina</option> <option value="Burundi">Burundi</option>
+                <option value="Canada">Canada</option> <option value="Madagascar">Madagascar</option> <option value="Nepal">Nepal</option>
+                <option value="Morocco">Morocco</option> <option value=""></option> <option value="Malaysia">Malaysia</option>
+                <option value="Chad">Chad</option> <option value="Libya">Libya</option> <option value="Iraq">Iraq</option>
+                <option value="Bolivia">Bolivia</option> <option value="Rwanda">Rwanda</option> 
+                </Form.Control>
+            </Form.Group>
+            {errors.description?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
+
+        </div>
+    )
+
+}
