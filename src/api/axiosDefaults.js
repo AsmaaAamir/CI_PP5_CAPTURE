a569b33773dd73244064aefbb2dcc45ff844f407
AsmaaAamir <>
2023-06-02 06:36:37 +0000

    added axios interceptors so user doesntbhave come back loggin eveytime it refreshes

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://git.heroku.com/capture-api-drf.git'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = "https://git.heroku.com/capture-api-drf.git";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
+
