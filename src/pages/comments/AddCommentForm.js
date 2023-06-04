dd56ccef49bc95a0d81300d489afa658ec7104aa
AsmaaAamir <>
2023-06-04 04:57:23 +0000

    Added form fro comments to be added on the post

---
+++ src/pages/comments/AddCommentForm.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Form, InputGroup } from "react-bootstrap";
+import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
+
+
+function AddCommentForm(props){
+
+    const { post, setPost, setComments, 
+            ProfileImage, profile_id } = props;
+    const [content, setContent] = useState("");
+
+    const handleChange = (e) => {
+        setContent(e.target.value);
+    };
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const { data } = await axiosRes.post("/comments/", { 
+                content, post, });
+            
+            setComments((prevComments) => ({
+                ...prevComments,
+                results: [data, ...prevComments.results],
+            }));
+            setPost((prevPost) =>({ 
+                results: [ {
+                    ...prevPost.results[0],
+                    comments_count: prevPost.result[0].comments_count + 1,
+                },
+            ],
+        }));
+        setContent("");
+        } catch (err){
+            console.log(err);
+        }
+    };
+    return (
+        <Form className="mt-2" onSubmit={handleSubmit}>
+            <Form.Group>
+                <InputGroup>
+                    <Link tp={'/profile/${profile_id}'}>
+                        <Avartar src={profileImage}/>
+                    </Link>
+                    <Form.Control
+                        className={style.Form}
+                        as="textarea"
+                        row={2}
+                        placeholder="Please type your comment..."
+                        value={content}
+                        onChange={handleChange}
+                    />
+                </InputGroup>
+            </Form.Group>
+            <button className={'${styles.Button} btn d-block ml-auto'}
+                disabled={!content.trim()} type="submit">
+                post
+            </button>
+        </Form>
+    );
+}
+
+export default AddCommentForm;
