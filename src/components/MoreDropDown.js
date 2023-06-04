8c2d937603f8a4651f2c8c63451cd1f7804688de
AsmaaAamir <>
2023-06-04 11:15:59 +0000

    added Profile page content

--- src/components/MoreDropDown.js
+++ src/components/MoreDropDown.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Dropdown} from "react-bootstrap";
+import { useHistory } from "react-router";
 
 const MoreOptions = React.forwardRef(({ onClick}, ref) => (
     <i className="fas- fa-ellipsis" ref={ref} onClick={
@@ -21,6 +22,26 @@ export const MoreDropdown = ({ handleEdit, handleDelet }) => {
                 <Drop.Item className={style.Dropdown} onClick={handleDelet} aria-label="Delet">
                     <i className="fas- fa-trash-alt" />
                 </Drop.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    );
+};
+
+export const ProfileEditDropdown = ({ id }) => {
+    const history = useHistory():
+    return (
+        <Dropdown className={'ml-auto- px-3 ${stlyes.Absolute}'} drop="left">
+            <Dropdown.Toggle as={MoreOptions} />
+            <Dropdown.Menu>
+                <Dropdown.Item onClick={() => history.push('/profiles/${id}/edit')} >
+                    <i className="fas fa-edit" /> Edit Profile 
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => history.push('/profiles/${id}/edit.username')} >
+                    <i className="fas id-card" /> Change Username 
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => history.push('/profiles/${id}/edit/password')} >
+                    <i className="fas fa-key" /> Change Password 
+                </Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     );
