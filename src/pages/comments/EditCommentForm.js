b4cc21e147f4256242cab5b4734cbdf6270ae597
AsmaaAamir <>
2023-06-04 05:17:01 +0000

    added comment edit form

---
+++ src/pages/comments/EditCommentForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap";
+import { axiosRes } from "../../api/axiosDefaults";
+
+function EditCommentForm(props) {
+
+    const {id, content, setShowEditForm, setComments} = props;
+    const [formContent, setFormContent] = useState(content);
+
+    const handleChange=  (e) => {
+        setFormContent(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDeafult();
+        try{
+            await axiosRes.put('/comments/${id}/', {
+                content: formContent.trim(),
+            });
+        setComments((prevComments) => ({
+            ...prevComments, 
+            results: prevComments.results.map((comment) => {
+                return comment.id === id ? {
+                    ...comment, 
+                    content: formContent.trim(),
+                    updatd_at: "now",
+                }
+                : comment;
+            }),
+        }));
+        setShowEditForm(false);
+        } catch (err){
+            console.log(err);
+        }    
+    };
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Form.Group className="pr-1">
+                <Form.Control 
+                    as="textarea"
+                    row={2}
+                    value={formContent}
+                    className={styles.Form}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            <div className="text-right">
+                <button className={styles.Button} onClick={() => setShowEdirForm(false)}
+                    type="button"> Cancel </button>
+                <button className={styles.Button} disabled={!content.trim()}
+                    type="button"> Save </button>
+            </div>
+        </Form>
+    );
+}
+
+export default EditCommentForm;
