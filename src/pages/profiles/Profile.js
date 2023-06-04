a40468a598bb2b5c4b748955928699a04bc78936
AsmaaAamir <>
2023-06-04 05:54:32 +0000

    completed profile

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -2,7 +2,7 @@ import React from "react";
 import  { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
-import { useSetProfileData } "../../contexts/ProfileDataContext";
+import { useSetProfileData } from "../../contexts/ProfileDataContext";
 
 const Profile = (props) => { 
     const { profile, mobile, imageSize = 55} = props;
@@ -12,6 +12,30 @@ const Profile = (props) => {
     const { handleFollow, handleUnfollow } = useSetProfileData();
 
     return(
-        
-    )
-}
+        <div calssName={'my-3 d-flex align-item-center ${modile && "flex=column"}'}>
+            <div>
+                <Link to={'/profile/${id}'}className="align-self-center">
+                    <Avatar src={image} height={imageSize}/>
+                </Link>
+            </div>
+            <div className={'mx-2 ${styles.Break}'}>
+                <strong>{owner}</strong>
+            </div>
+            <div className={'text=right ${!mobile && "ml-auto"}'}>
+                {!mobile && currentUser && !is_owner && (following_id ? (
+                    <Button className={'${styles.Button} ${styles.BlackOutline}'}
+                        onClick={() => handleUnfollow(profile)}>
+                            Unfollow 
+                    </Button>
+                ) : (
+                    <Button className={'${styles.Button} ${styles.Black}'}
+                        onClick={() => handleFollow(profile)}>
+                            Follow 
+                    </Button>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
